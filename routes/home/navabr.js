3c2cbf7a127bfd7a12bfdcbbfbc9f21fe2be6596
nakulmahajan.ac.in@gmail.com <nakulmahajan.ac.in@gmail.com>
2025-10-17 03:17:03 +0530

    chatbot added

--- routes/home/navabr.js
+++ routes/home/navabr.js
@@ -1,27 +1,67 @@
-const express= require("express" );
+const express = require("express");
 const router = express.Router();
-const ClassMonthlyFee = require('../../models/ClassMonthlyFee');
+const axios = require("axios"); // <-- Add this line
+const ClassMonthlyFee = require("../../models/ClassMonthlyFee");
 
-router.get('/index', (req, res) => {
-  res.render('index');
+// ------------------------
+// Static page routes
+// ------------------------
+router.get("/index", (req, res) => {
+  res.render("index");
 });
-router.get('/admission', (req, res) => {
-  res.render('navbar/admission'); 
+
+router.get("/admission", (req, res) => {
+  res.render("navbar/admission");
 });
-router.get('/contactus', (req, res) => {
-  res.render('navbar/contactus'); 
+
+router.get("/contactus", (req, res) => {
+  res.render("navbar/contactus");
 });
-router.get('/aboutus', (req, res) => {
-  res.render('navbar/aboutus'); 
+
+router.get("/aboutus", (req, res) => {
+  res.render("navbar/aboutus");
 });
-router.get('/download/HW', (req, res) => {
-  res.render('navbar/downloadHW'); 
+
+router.get("/download/HW", (req, res) => {
+  res.render("navbar/downloadHW");
 });
-router.get('/transport', (req, res) => {
-  res.render('navbar/transport');
+
+router.get("/transport", (req, res) => {
+  res.render("navbar/transport");
 });
-router.get('/feeStructure', async(req, res) => {
-   const classFee  = await ClassMonthlyFee.find({});
-  res.render('navbar/feeStructure' , {classFee} );
+
+router.get("/feeStructure", async (req, res) => {
+  const classFee = await ClassMonthlyFee.find({});
+  res.render("navbar/feeStructure", { classFee });
 });
-module.exports = router ;
+
+router.get("/chat", (req, res) => {
+  res.render("navbar/chat" , { showFooter: false}); // this page will contain the chat form
+});
+
+// ------------------------
+// RAG Chatbot query route
+// ------------------------
+router.post("/query", async (req, res) => {
+  const { userQuery } = req.body;
+
+  if (!userQuery) {
+    return res.status(400).json({ error: "Query is required" });
+  }
+
+  try {
+    // Call Python RAG FastAPI endpoint
+    const response = await axios.post("http://16.171.33.53:8000/ask", {
+      query: userQuery,
+      top_k: 5,
+    });
+
+    // Return the answer as JSON to frontend
+    res.json({ answer: response.data.answer });
+  } catch (err) {
+    console.error("RAG API error:", err.message);
+    res.status(500).json({ error: "Failed to get answer from portal assistant" });
+  }
+});
+
+module.exports = router;
